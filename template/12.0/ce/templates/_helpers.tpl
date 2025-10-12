haiwen/seafile-helm-chart

commit 8215b0b14bb6be272b241844fff219ff84f6c54e

update seafile helm chart
* update Seafile Helm Chart

* update chart version & publish new versions

* link the newest publish

* publish

* add disablePV

template/12.0/ce/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

template/12.0/cluster/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

template/12.0/pro/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

template/13.0/ce/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

template/13.0/cluster/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

template/13.0/pro/templates/_helpers.tpl
@@ -9,6 +9,18 @@
 
 {{/* Check volume use or set by default  */}}
 
+{{- define "seafile.seafileDataVolume.enabled" -}}
+    {{- $config := .Values.seafile.configs.seafileDataVolume -}}
+    {{- if or 
+        (not $config) 
+        (eq ($config.disablePVC | default false) false)
+    }}
+        true
+    {{- else }}
+        false
+    {{- end }}
+{{- end }}
+
 {{/* for storage size  */}}
 {{- define "seafile.seafileDataVolume.storage" -}}
     {{- if and (.Values.seafile.configs.seafileDataVolume) (.Values.seafile.configs.seafileDataVolume.storage) }}

commit 39c7bf0602b15f9ef4c791aaccc5c64870adb252

fix: whitespace bug in _helpers.tpl causing PVC to never be created
This fixes a critical bug introduced in 8215b0b14bb6be272b241844fff219ff84f6c54e
where the `seafile.seafileDataVolume.enabled` template helper returns
"true" or "false" with leading whitespace, causing the string comparison
`| eq "true"` to always fail.

The bug prevented PersistentVolumeClaims from being created in all cases
where they should be created (when disablePVC is absent or set to false).

Changes:
- Added `-}}` trimming syntax after the if condition (line 17)
- Added `-}}` trimming syntax after the else clause (line 19)
- This ensures "true" and "false" are returned without whitespace

Files fixed:
- template/12.0/ce/templates/_helpers.tpl
- template/12.0/cluster/templates/_helpers.tpl
- template/12.0/pro/templates/_helpers.tpl
- template/13.0/ce/templates/_helpers.tpl
- template/13.0/cluster/templates/_helpers.tpl
- template/13.0/pro/templates/_helpers.tpl

template/12.0/ce/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 

template/12.0/cluster/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 

template/12.0/pro/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 

template/13.0/ce/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 

template/13.0/cluster/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 

template/13.0/pro/templates/_helpers.tpl
@@ -14,10 +14,10 @@
     {{- if or 
         (not $config) 
         (eq ($config.disablePVC | default false) false)
-    }}
-        true
-    {{- else }}
-        false
+    -}}
+    true
+    {{- else -}}
+    false
     {{- end }}
 {{- end }}
 